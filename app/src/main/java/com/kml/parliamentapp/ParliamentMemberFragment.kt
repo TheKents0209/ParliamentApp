6586aaf5c313dff97fb20ec0309fc2ce9b2aaed1
Kenert <kenert2001@gmail.com>
2021-02-09 00:28:59 +0200

    Added parliament member data and getting random member and showing it's info

--- app/src/main/java/com/kml/parliamentapp/ParliamentMemberFragment.kt
+++ app/src/main/java/com/kml/parliamentapp/ParliamentMemberFragment.kt
@@ -11,10 +11,21 @@ import com.kml.parliamentapp.databinding.FragmentParliamentMemberBinding
 
 class ParliamentMemberFragment : Fragment() {
 
+    private var randomMember = members.randomMember()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentParliamentMemberBinding>(inflater,
             R.layout.fragment_parliament_member,container,false)
+
+        randomMember?.let {
+            binding.apply {
+                member = randomMember
+                invalidateAll()
+            }
+        }
+        binding.memberFirstNameTV.text = randomMember.firstname
+
         return binding.root
     }
 
