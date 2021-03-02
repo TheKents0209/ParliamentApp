25062760eca9e8e0aa3afa2aa523d93369c76901
Kenert <kenert2001@gmail.com>
2021-03-02 16:26:51 +0200

    New model for storing likes value in

--- app/src/main/java/com/kml/parliamentapp/data/model/Likes.kt
+++ app/src/main/java/com/kml/parliamentapp/data/model/Likes.kt
@@ -1,6 +1,11 @@
 package com.kml.parliamentapp.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "likes_table")
 data class Likes(
+    @PrimaryKey
     val hetekaId: Int,
-    var likes: Int
+    var likes: Int = 0
 )
