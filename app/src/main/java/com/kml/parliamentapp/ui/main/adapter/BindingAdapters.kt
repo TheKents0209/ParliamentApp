05becb08240cf6aedfb7fb4999e7d14d217d9ed7
Kenert <kenert2001@gmail.com>
2021-03-03 00:41:00 +0200

    binding adapters that convert shortened versions of party names to full names, also party logos in list

--- app/src/main/java/com/kml/parliamentapp/ui/main/adapter/BindingAdapters.kt
+++ app/src/main/java/com/kml/parliamentapp/ui/main/adapter/BindingAdapters.kt
@@ -2,9 +2,13 @@ package com.kml.parliamentapp.ui.main.adapter
 
 import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.kml.parliamentapp.R
+import com.kml.parliamentapp.data.model.ParliamentMember
+import com.kml.parliamentapp.data.model.Party
 
 //Is called when XML contains value imageUrl
 @BindingAdapter("imageUrl")
@@ -16,4 +20,52 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
 
         Glide.with(imgView.context).load(imgUri).into(imgView)
     }
+}
+
+@BindingAdapter("partyLogo")
+fun ImageView.bindLogo(item: Party) {
+    setImageResource(when (item.party) {
+        "kesk" -> R.mipmap.ic_kesk_logo
+        "sd" -> R.mipmap.ic_sdp_logo
+        "vihr" -> R.mipmap.ic_vihr_logo
+        "kok" -> R.mipmap.ic_kok_logo
+        "ps" -> R.mipmap.ic_ps_logo
+        "r" -> R.mipmap.ic_r_logo
+        "vas" -> R.mipmap.ic_vas_logo
+        "kd" -> R.mipmap.ic_kd_logo
+        "liik" -> R.mipmap.ic_liik_logo
+        else -> R.drawable.ic_launcher_background
+    })
+}
+
+@BindingAdapter("partyName")
+fun TextView.bindPartyName(item: Party) {
+    text = (when (item.party) {
+        "sd" -> "Suomen Sosialidemokraattinen Puolue"
+        "vihr" -> "Vihreä liitto"
+        "kok" -> "Kansallinen Kokoomus"
+        "ps" -> "Perussuomalaiset"
+        "r" -> "Suomen ruotsalainen kansanpuolue"
+        "kesk" -> "Suomen Keskusta"
+        "vas" -> "Vasemmistoliitto"
+        "kd" -> "Suomen Kristillisdemokraatit"
+        "liik" -> "Liike Nyt"
+        else -> "Error"
+    })
+}
+
+@BindingAdapter("partyNameMember")
+fun TextView.bindPartyNameMember(item: String) {
+    text = (when (item) {
+        "sd" -> "Suomen Sosialidemokraattinen Puolue"
+        "vihr" -> "Vihreä liitto"
+        "kok" -> "Kansallinen Kokoomus"
+        "ps" -> "Perussuomalaiset"
+        "r" -> "Suomen ruotsalainen kansanpuolue"
+        "kesk" -> "Suomen Keskusta"
+        "vas" -> "Vasemmistoliitto"
+        "kd" -> "Suomen Kristillisdemokraatit"
+        "liik" -> "Liike Nyt"
+        else -> "Error"
+    })
 }
