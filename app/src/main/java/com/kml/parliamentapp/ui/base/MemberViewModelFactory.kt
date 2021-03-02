f510104c498f6aaba16dcbe2dbc08091ae95ad4c
Kenert <kenert2001@gmail.com>
2021-03-02 19:03:31 +0200

    added extra parameter

--- app/src/main/java/com/kml/parliamentapp/ui/base/MemberViewModelFactory.kt
+++ app/src/main/java/com/kml/parliamentapp/ui/base/MemberViewModelFactory.kt
@@ -3,19 +3,22 @@ package com.kml.parliamentapp.ui.base
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.kml.parliamentapp.data.database.LikesDatabaseDao
 import com.kml.parliamentapp.data.database.MemberDatabaseDao
+import com.kml.parliamentapp.data.repository.LikesRepository
 import com.kml.parliamentapp.data.repository.MemberRepository
 import com.kml.parliamentapp.ui.main.viewmodel.MemberViewModel
 
 class MemberViewModelFactory(
     private val databaseDao: MemberDatabaseDao,
+    private val likesDatabaseDao: LikesDatabaseDao,
     private val application: Application,
     private val hetekaIdKey: Int
 ) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MemberViewModel::class.java)) {
-            return MemberViewModel(MemberRepository(databaseDao), hetekaIdKey) as T
+            return MemberViewModel(MemberRepository(databaseDao),LikesRepository(likesDatabaseDao), hetekaIdKey) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
