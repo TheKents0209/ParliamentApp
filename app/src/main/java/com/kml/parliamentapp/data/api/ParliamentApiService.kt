51f8d8d27ee93a3b393383d0707391a6ace306f8
Kenert <kenert2001@gmail.com>
2021-03-03 17:59:54 +0200

    Uses correct URL now

--- app/src/main/java/com/kml/parliamentapp/data/api/ParliamentApiService.kt
+++ app/src/main/java/com/kml/parliamentapp/data/api/ParliamentApiService.kt
@@ -14,10 +14,8 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-
-//TODO: Use https://avoindata.eduskunta.fi/api/v1/seating/ instead.
 private const val BASE_URL =
-    "http://users.metropolia.fi/~kenertml/"
+    "https://avoindata.eduskunta.fi/api/v1/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -30,7 +28,7 @@ private val retrofit = Retrofit.Builder()
 
 
 interface ParliamentApiService {
-    @GET("eduskunta")
+    @GET("seating/")
     suspend fun getMembers(): List<ParliamentMember>
 }
 
